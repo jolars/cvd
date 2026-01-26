317f69ee818f8204c78703b82d0b2c7aa7dd6a85
Johan Larsson <johan@jolars.co>
2026-01-26 14:22:01 +0100

    refactor: switch to devenv for develoment

---
+++ devenv.nix
@@ -0,0 +1,93 @@
+{
+  pkgs,
+  ...
+}:
+let
+  texlive = pkgs.texliveMedium.withPackages (
+    ps: with ps; [
+      scheme-basic
+      l3build
+      beamer
+      biblatex
+      enumitem
+      fileinfo
+      hypdoc
+      hyperref
+      listings
+      metalogo
+      parskip
+      pgf
+      pgfopts
+      setspace
+      xurl
+      microtype
+      koma-script
+      booktabs
+      minted
+      xcolor
+      upquote
+      mdwtools
+      caption
+      float
+      fancyvrb
+      tcolorbox
+      tikzfill
+      pdfcol
+      fontawesome5
+    ]
+  );
+  l3build-wrapped = pkgs.writeShellScriptBin "l3build-wrapped" ''
+    # NOTE: the trailing slash in TEXMF is required
+    TEXMF="${texlive}/" ${texlive}/bin/l3build "$@"
+  '';
+in
+{
+  packages = [
+    pkgs.git
+    pkgs.bashInteractive
+    pkgs.pandoc
+    pkgs.quartoMinimal
+    l3build-wrapped
+    pkgs.go-task
+    pkgs.poppler-utils
+  ];
+
+  languages = {
+    texlive = {
+      enable = true;
+      base = texlive;
+    };
+
+    javascript = {
+      enable = true;
+
+      corepack.enable = true;
+
+      pnpm = {
+        enable = true;
+
+        install = {
+          enable = true;
+        };
+      };
+    };
+
+    typescript = {
+      enable = true;
+    };
+  };
+
+  treefmt = {
+    enable = true;
+
+    config.programs = {
+      nixfmt.enable = true;
+
+      stylua.enable = true;
+    };
+  };
+
+  git-hooks.hooks = {
+    treefmt.enable = true;
+  };
+}
